d1f43a2ed32f6c1b94acedf8cba666ac9f2fb0c8
KevAkaSlayer <pukman41@gmail.com>
2024-03-30 15:12:02 +0600

    done

--- Wellness_Oasis_Clinic/settings.py
+++ Wellness_Oasis_Clinic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(yxlmfq--&$vwd^fazyvbmlnfz0657s7+ju7dyzh3%*9(2yuf-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
