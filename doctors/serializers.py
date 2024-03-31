6724d62e92d9b4f22f02762133c133b5bba1f556
KevAkaSlayer <pukman41@gmail.com>
2024-03-31 17:39:54 +0600

    updated doctor and review serializer

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -18,17 +18,18 @@ class AvailableTimeSerializer(serializers.ModelSerializer):
         model = AvailableTime
         fields = '__all__'
 class DoctorSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField(many = False)
-    # designation = serializers.StringRelatedField(many = True)
-    # specialization = serializers.StringRelatedField(many = True)
-    # available_time = serializers.StringRelatedField(many = True)
+    user = serializers.StringRelatedField(many = False)
+    designation = serializers.StringRelatedField(many = True)
+    specialization = serializers.StringRelatedField(many = True)
+    available_time = serializers.StringRelatedField(many = True)
     class Meta:
         model = Doctor
         fields = '__all__'
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-
+    reviewer = serializers.StringRelatedField(many = False)
+    doctor = serializers.StringRelatedField(many = False)
     class Meta:
         model = Review
         fields = '__all__'
